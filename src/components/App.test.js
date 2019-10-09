8356386c7712ceebdd708e9ddb167fc4780e348c
Jeremy Greer <jex.grizzle@gmail.com>
2019-10-08 20:53:46 -0700

    Add Enzyme

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import {shallow} from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const factory = (props = {}) => {
+  return (
+    <App
+      {...props}
+    />
+  )
+}
+
+describe('App', () => {
+  it('should render', () => {
+    shallow(factory())
+  })
+})
